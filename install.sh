e4461d9408849b9178d5dc7dd0d97a0dce8fca58
Jamie Lentin <jm@lentin.co.uk>
2020-01-02 17:05:59 +0000

    install.sh: Systemd machinery to run workers

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -ex
+
+# ---------------------------
+# Config options, to override any of these, set them in .local.conf
+[ -e ".local-conf" ] && . ./.local-conf
+
+PROJECT_PATH="${PROJECT_PATH-$(dirname "$(readlink -f "$0")")}"  # The full project path, e.g. /srv/xxx
+PROJECT_NAME="${PROJECT_NAME-$(basename "${PROJECT_PATH}")}"  # The project directory name, e.g. xxx
+PROJECT_MODE="${PROJECT_MODE-development}"  # The project mode, development or production
+
+if [ "${PROJECT_MODE}" = "development" ]; then
+    SERVICE_INSTANCES="${SERVICE_INSTANCES-}"
+else
+    SERVICE_INSTANCES="${SERVICE_INSTANCES-1 2 3}"
+fi
+
+# -----------------
+# Systemd unit file
+
+systemctl stop --all "${PROJECT_NAME}@*.service"
+systemctl list-units --no-pager --no-legend -a "${PROJECT_NAME}@*.service" \
+    | cut -d' ' -f1 \
+    | xargs --no-run-if-empty systemctl disable
+
+cat <<EOF > "/etc/systemd/system/${PROJECT_NAME}.env"
+EOF
+chmod 600 -- "/etc/systemd/system/${PROJECT_NAME}.env"
+cat <<EOF > "/etc/systemd/system/${PROJECT_NAME}@.service"
+[Unit]
+Description=${PROJECT_NAME} instance %I
+After=network.target
+
+[Service]
+ExecStart=$(command -v Rscript) ${PROJECT_PATH}/worker.R
+WorkingDirectory=${PROJECT_PATH}
+DynamicUser=yes
+EnvironmentFile=/etc/systemd/system/${PROJECT_NAME}.env
+Restart=on-failure
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+for x in ${SERVICE_INSTANCES}; do
+    systemctl enable "${PROJECT_NAME}@${x}.service"
+    systemctl start "${PROJECT_NAME}@${x}.service"
+done
